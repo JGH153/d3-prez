d90049da0cb59c20d0a7fed46ce4d2f8c8606c08
JGH153 <jan.greger@gmail.com>
2020-05-29 15:12:01 +0200

    Bar chart done

--- demo/lerning/part 4/index.js
+++ demo/lerning/part 4/index.js
@@ -5,42 +5,67 @@ const svg = d3
   .attr("width", 600)
   .attr("height", 600);
 
+const margin = { top: 20, right: 20, bottom: 100, left: 100 };
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg
+  .append("g")
+  .attr("width", graphWidth)
+  .attr("height", graphHeight)
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+const xAxisGroup = graph
+  .append("g")
+  .attr("transform", `translate(0, ${graphHeight})`);
+const yAxisGroup = graph.append("g");
+
 d3.json("menu.json").then((data) => {
   const min = d3.min(data, (current) => current.orders);
   const max = d3.max(data, (current) => current.orders);
   const extent = d3.extent(data, (current) => current.orders);
-  console.log(min, max, extent);
+  // console.log(min, max, extent);
 
-  const xAxis = d3
+  // new name?
+  const xScaleAxis = d3
     .scaleBand()
     .domain(data.map((current) => current.name))
-    .range([0, 800])
+    .range([0, 500])
     .paddingInner(0.2)
     .paddingOuter(0.2);
 
-  const yAxis = d3
+  const yScaleAxis = d3
     .scaleLinear()
-    .domain([min - 20, max])
-    .range([0, 500]);
+    .domain([0, d3.max(data, (d) => d.orders)])
+    .range([graphHeight, 0]);
 
   //console.log(y(600))
 
   // join the data to circs
-  const rects = svg.selectAll("rect").data(data);
+  const rects = graph.selectAll("rect").data(data);
 
   // add attrs to circs already in the DOM
   rects
-    .attr("width", xAxis.bandwidth)
-    .attr("height", (data) => yAxis(data.orders))
+    .attr("width", xScaleAxis.bandwidth)
+    .attr("height", (data) => graphHeight - yScaleAxis(data.orders))
     .attr("fill", "orange")
-    .attr("x", (data, i) => xAxis(data.name));
+    .attr("x", (data, i) => xScaleAxis(data.name));
 
   // append the enter selection to the DOM
   rects
     .enter()
     .append("rect")
-    .attr("width", xAxis.bandwidth)
-    .attr("height", (data) => yAxis(data.orders))
+    .attr("width", xScaleAxis.bandwidth)
+    .attr("height", (data) => graphHeight - yScaleAxis(data.orders))
     .attr("fill", "orange")
-    .attr("x", (data, i) => xAxis(data.name));
+    .attr("x", (data, i) => xScaleAxis(data.name))
+    .attr("y", (data, i) => yScaleAxis(data.orders));
+
+  const xAxis = d3.axisBottom(xScaleAxis);
+  const yAxis = d3.axisLeft(yScaleAxis).ticks(3).tickFormat(i => i + ' Orders');
+
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
+
+  xAxisGroup.selectAll('text').attr("transform", `rotate(40)`).attr('text-anchor', 'start').attr('fill', 'orange');
 });
