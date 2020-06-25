a0b47a8bb6b2bfe5f706349c700ef512e91aa4c7
JGH153 <jan.greger@gmail.com>
2020-06-25 22:59:21 +0200

    Update and remove

--- demo/lerning/part 8 -donut chart/graph.js
+++ demo/lerning/part 8 -donut chart/graph.js
@@ -15,11 +15,11 @@ const pie = d3
   .sort(null)
   .value((data) => data.cost);
 
-const angles = pie([
-  { name: "rent", cost: 500 },
-  { name: "bills", cost: 300 },
-  { name: "gaming", cost: 200 },
-]);
+// const angles = pie([
+//   { name: "rent", cost: 500 },
+//   { name: "bills", cost: 300 },
+//   { name: "gaming", cost: 200 },
+// ]);
 
 const arcPath = d3
   .arc()
@@ -29,17 +29,21 @@ const arcPath = d3
 // console.log(arcPath(angles[0])); //M9.184850993605149e-15,-150A150,150,0,1,1,9.184850993605149e-15,150LNaN,NaNZ
 
 const color = d3.scaleOrdinal(d3.schemeSet3);
-console.log("d3", d3);
+// console.log("d3", d3);
 
 // update function
 const update = (data) => {
+  // update the color scale domain
+  color.domain(data.map((current) => current.name));
 
-	// update the color scale domain
-	color.domain(data.map(current => current.name))
-
-  console.log(data);
   // join enhanced (pie) data to path elements
   const paths = graph.selectAll("path").data(pie(data));
+
+  // 3 remove exit selection
+  paths.exit().remove();
+
+  // update existing
+  paths.attr("d", arcPath).attr("fill", (current) => color(current.data.name));
 
   // paths.enter() is a for each map
   paths
@@ -48,10 +52,8 @@ const update = (data) => {
     .attr("class", "arc")
     .attr("d", arcPath)
     .attr("stroke", "#fff")
-		.attr("stroke-width", 3)
-		.attr('fill', current => color(current.data.name) )
-
-  console.log(paths.enter());
+    .attr("stroke-width", 3)
+    .attr("fill", (current) => color(current.data.name));
 };
 
 const data = [];
